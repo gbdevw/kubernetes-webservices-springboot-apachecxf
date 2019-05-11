c6aa4885de5f427218f38f57d698d5e8d2ed00b4
Braibant Guillaume <guillaume.braibant@hepl.be>
2019-05-11 17:34:29 +0200

    Upd. gitignore + setup project with wsdl & pom.xml

---
+++ src/main/java/guillaume_braibant/webservices_k8s_springboot_apachecxf/types/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservices-k8s-springboot-apachecxf.guillaume-braibant/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package guillaume_braibant.webservices_k8s_springboot_apachecxf.types;
